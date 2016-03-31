c596d5f36e21eb4147140b51ead183791a983fee
why <hy1121@foxmail.com>
2016-03-31 08:38:10 +0800

    测试提交

---
+++ ItcastOA/src/cn/itcast/oa/dao/DepartmentDao.java
@@ -0,0 +1,9 @@
+package cn.itcast.oa.dao;
+
+import cn.itcast.oa.base.BaseDao;
+import cn.itcast.oa.base.BaseDaoImpl;
+import cn.itcast.oa.domain.Department;
+
+public class DepartmentDao extends BaseDaoImpl<Department> implements BaseDao<Department> {
+
+}
